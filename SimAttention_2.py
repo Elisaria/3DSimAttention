6964fc3fe83c027746f0e1c024dacb437054258e
Elisaria <120236643+Elisaria@users.noreply.github.com>
2025-09-21 23:27:48 +0800

    3DSimAttention.py
    This is the final optimized version of 3DSimAttention, which supports both multi-head and single-head modes.

--- SimAttention_2.py
+++ SimAttention_2.py
@@ -4,28 +4,26 @@
 # k : the number of ramdon mapping functions
 # ==============================================================================
 
-import numpy as np
 import tensorflow as tf
-import ClusterPartition as CP
-import time
+from keras import layers
 
 
 # Perform max pooling on the neighborhood of the sampling points
 # to extract significant features and, simultaneously,
 # make the neighborhood features possess order invariance.
-class Neighborhood_MaxPooling(tf.keras.layers.Layer):
+class Neighborhood_MaxPooling(layers.Layer):
 
     def __init__(self, channels, **kwargs):
         super().__init__(**kwargs)
         # High-Rank Mapping
         # Minimize the loss of feature information caused by Max Pooling
-        self.HR_dense = tf.keras.layers.Dense(2 * channels, activation='relu')
+        self.HR_dense = layers.Dense(2 * channels, activation='relu')
         # Low-Rank Mapping
         # Reduce the input burden of subsequent network layers
-        self.LR_dense = tf.keras.layers.Dense(channels, activation='relu')
+        self.LR_dense = layers.Dense(channels, activation='relu')
 
     def build(self, input_shape):
-        self.MaxPool = tf.keras.layers.MaxPool2D(pool_size=[1, input_shape[-2]], strides=1)
+        self.MaxPool = layers.MaxPool2D(pool_size=[1, input_shape[-2]], strides=1)
 
     # n : Number of sampling points
     # m : Number of points in neighborhood of a sampling point
@@ -43,7 +41,7 @@ class Neighborhood_MaxPooling(tf.keras.layers.Layer):
         return x
 
 
-class Down_Projection(tf.keras.layers.Layer):
+class Down_Projection(layers.Layer):
     def __init__(self, d_l, **kwargs):
         super().__init__(**kwargs)
         self.d_l = d_l
@@ -60,7 +58,7 @@ class Down_Projection(tf.keras.layers.Layer):
 
 
 # obtain the random mapping table
-class Random_Projection(tf.keras.layers.Layer):
+class Random_Projection(layers.Layer):
     # k : number of ramdom projection functions
     def __init__(self, k, **kwargs):
         super().__init__(**kwargs)
@@ -103,24 +101,24 @@ def Memory_Reorganization(activations, RMVs, k_h):
 # Based on SimValues,
 # a neighborhood feature sequence with local correlation is constructed,
 # thereby obtaining attention in the form of a sliding window.
-class SimAttention(tf.keras.layers.Layer):
+class SimAttention(layers.Layer):
     # The window size must be odd
     # If the window_size is equal to input_shape[1], it degenerates into full attention.
     def __init__(self, d_l, d_h, k_h, k, window_size=9, **kwargs):
         super().__init__(**kwargs)
-        self.reshape = tf.keras.layers.Reshape((-1, window_size, d_l))
-        self.concat = tf.keras.layers.Concatenate(axis=1)
+        self.reshape = layers.Reshape((-1, window_size, d_l))
+        self.concat = layers.Concatenate(axis=1)
         if window_size % 2 != 1:
             raise ValueError("The window size must be odd !")
 
         self.window_size = window_size
         self.k_h = k_h
-        self.DOWN_PROJECTION_1 = tf.keras.layers.Dense(d_l, activation='relu')
+        self.DOWN_PROJECTION_1 = layers.Dense(d_l, activation='relu')
         self.DOWN_PROJECTION_2 = Down_Projection(d_l)
         self.RANDOM_PROJECTION = Random_Projection(k)
-        self.UP_PROJECTION = tf.keras.layers.Dense(d_h, activation='relu')
+        self.UP_PROJECTION = layers.Dense(d_h, activation='relu')
 
-        self.LAYERNORM = tf.keras.layers.LayerNormalization()
+        self.LAYERNORM = layers.LayerNormalization()
 
     def build(self, input_shape):
         if input_shape[1] < self.window_size:
