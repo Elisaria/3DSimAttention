5f40d116cc982094228c02dc0f0d3f151ddce978
Elisaria <120236643+Elisaria@users.noreply.github.com>
2025-09-22 13:23:49 +0800

    Update Segmentation_Model.py

--- Segmentation_Model.py
+++ Segmentation_Model.py
@@ -1,6 +1,4 @@
 import tensorflow as tf
-import numpy as np
-from tensorflow import keras
 from keras import layers
 import SimAttention as SA
 
@@ -20,36 +18,8 @@ class model(tf.keras.Model):
         return self.seg_head(x)  # (B, 2024, 2)
 
 
-class ExportModel(tf.Module):
-    def __init__(self, model):
-        self.model = model
-
-    @tf.function(input_signature=[tf.TensorSpec(shape=[100, 2048, 3], dtype=tf.float32)])
-    def __call__(self, inputs):
-        result = self.model(inputs, training=False)
-
-        return result
-
-
+# Forward propagation
 if __name__ == '__main__':
-    train_data = 'data address'
-    val_data = 'data address'
-    train_labels = 'data address'
-    val_labels = 'data address'
-    dataset = tf.data.Dataset.from_tensor_slices((train_data, train_labels)).batch(32)
-    val = tf.data.Dataset.from_tensor_slices((val_data, val_labels)).batch(32)
-
-    model = model(layer_num=4)
-    model.compile(optimizer=keras.optimizers.AdamW(learning_rate=0.0001,
-                                                   weight_decay=0.01,
-                                                   beta_1=0.9,
-                                                   beta_2=0.95
-                                                   ),
-                  loss=keras.losses.sparse_categorical_crossentropy,
-                  metrics=['accuracy'])
-    model.fit(dataset,
-              validation_data=val,
-              epochs=1500)
-
-    export_model = ExportModel(model)
-    tf.saved_model.save(export_model, export_dir='ModelNet40_model')
+    inputs = tf.random.normal([2, 2048, 3])
+    seg_model = model(layer_num=4)
+    print(seg_model(inputs).shape)
