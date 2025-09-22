43b1d530a925d2a00d0d286ae61c5ce1be34cd23
Elisaria <120236643+Elisaria@users.noreply.github.com>
2025-09-22 12:30:45 +0800

    Update Classification_Model.py
    This is a classification model case based on the ModelNet40 dataset. The input point cloud P has a shape of \((\text{batch_size}=32, L=2048, 3)\).When the Neighborhood-Wise (NW) paradigm is adopted:P first undergoes sampling and grouping to generate neighborhoods with a shape of \((\text{batch_size}, n=72, K=32, 3)\);Then, pooling is applied to obtain latent features with a shape of \((\text{batch_size}, n=72, d=32)\).When the Point-Wise (PW) paradigm is used:P is directly fed into 3DSimAttention without additional processing.Regardless of the paradigm adopted, the final output shape of the classification task remains \((\text{batch_size}, \text{class}=40)\).

--- Classification_Model.py
+++ Classification_Model.py
@@ -3,7 +3,7 @@ import numpy as np
 from tensorflow import keras
 from keras import layers
 import SimAttention as SA
-import Neighboorhood_Partition as NP
+import Neighborhood_Partition as NP
 
 
 class model(tf.keras.Model):
@@ -19,7 +19,7 @@ class model(tf.keras.Model):
     def call(self, x, training=None, mask=None):
         KPs = NP.FPS(x, 72)
         # x(B, 72, 32, 3)
-        x = NP.Neighboorhood_Partition(x, KPs, 32)
+        x = NP.Neighborhood_Partition(x, KPs, 32)
         # x(B, 72, 32)
         x = self.Neigh_pool(x)
         for i in range(self.layer_num):
@@ -27,7 +27,7 @@ class model(tf.keras.Model):
         x = self.globalMaxPool(x)  # (B, 72)
         return self.classify_head(x)  # (B, 40)
 
-
+# save model
 class ExportModel(tf.Module):
     def __init__(self, model):
         self.model = model
@@ -39,16 +39,12 @@ class ExportModel(tf.Module):
 
 
 if __name__ == '__main__':
-    data = np.load("ModelNet40/augmented_train.npy")
-    val_data = np.load("ModelNet40/test_data.npy")
-    print(data.shape)
-    print(val_data.shape)
-    labels = np.load("ModelNet40/augmented_label.npy")
-    print(labels.shape)
-    val_labels = np.load("ModelNet40/test_labels.npy")
-    print(val_labels)
-    dataset = tf.data.Dataset.from_tensor_slices((data, labels)).batch(16)
-    val = tf.data.Dataset.from_tensor_slices((val_data, val_labels)).batch(16)
+    train_data = 'data address'
+    val_data = 'data address'
+    train_labels = 'data address'
+    val_labels = 'data address'
+    dataset = tf.data.Dataset.from_tensor_slices((train_data, train_labels)).batch(32)
+    val = tf.data.Dataset.from_tensor_slices((val_data, val_labels)).batch(32)
 
     model = model(layer_num=4)
     model.compile(optimizer=keras.optimizers.AdamW(learning_rate=0.0001,
